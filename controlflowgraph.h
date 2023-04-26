bbb72fdb6ab860872b921eb16b40501b43ddefb7
Maple_of_Ten <75062138+shifengzhicheng@users.noreply.github.com>
2023-04-26 22:45:55 +0800

    Update controlflowgraph.h

--- controlflowgraph.h
+++ controlflowgraph.h
@@ -4,8 +4,8 @@
 
 struct graph_node {
 	DataFlowGraph DFG;
-	std::vector<OutputEdge> controlnodes;//相关的边
-	graph_node(DataFlowGraph &_DFG): DFG(_DFG), controlnodes(DFG.get_outputEdge()) {}
+	std::vector<BranchEdge> controlnodes;//相关的边
+	graph_node(DataFlowGraph &_DFG): DFG(_DFG), controlnodes(DFG.get_Branches()) {}
 };
 
 class ControlFlowGraph {
@@ -40,13 +40,11 @@ class ControlFlowGraph {
 			// 首先生成一个空块来装函数的输入变量
 			{
 				DataFlowGraph dfg;
-				dfg.get_opList().push_back(op());
-				dfg.get_opList().push_back(op());
 				for (int i = 0; i < vars.size(); i++) {
 					IndexofDFG[dfg.get_label()] = 0;
 					dfg.myOutvartable()[vars[i]._name] = 1;
 				}
-				dfg.get_outputEdge()[0].To_Block = p.get_basic_blocks()[0].get_label_name();
+				dfg.get_Branches()[0].To_Block = p.get_basic_blocks()[0].get_label_name();
 				DFGs.push_back(graph_node(dfg));
 			}
 			// 这一步是初步把块生成图
@@ -55,8 +53,8 @@ class ControlFlowGraph {
 				basic_block &bb = p.get_basic_blocks()[i];
 				DataFlowGraph DFG(bb);
 				IndexofDFG[DFG.get_label()] = currentBlock;
-				if (DFG.get_outputEdge().empty()) {
-					DFG.get_outputEdge().push_back(OutputEdge(DFG.get_label(),p.get_basic_blocks()[i+1].get_label_name(),UnConditonal));
+				if (DFG.get_Branches().empty()) {
+					DFG.get_Branches().push_back(BranchEdge(DFG.get_label(),p.get_basic_blocks()[i+1].get_label_name(),UnConditonal));
 				}
 				DFGs.push_back(graph_node(DFG));
 				// 到这里已经完成了分支出度的边OutputEdge
@@ -65,26 +63,28 @@ class ControlFlowGraph {
 			// 整理DFGs中的数据流关系，并根据DFG中的opList生成DFG的inputList
 			for (int i = 0; i < DFGs.size(); i++) {
 				//TODO
-				for (int j = 0; j < DFGs[i].DFG.get_inputList().size(); j++) {
+				DataFlowGraph& CurDFG = DFGs[i].DFG;
+				for (int j = 0; j < CurDFG.get_inputList().size(); j++) {
 					// 块中变量与外部块对应
-					if (DFGs[i].DFG.get_inputList()[j].From_Block.empty()) {
-						std::string label= findvarfrom(DFGs[i].DFG.get_inputList()[j].InputBlockVarName);
-						DFGs[i].DFG.get_inputList()[j].From_Block = label;
+					if (CurDFG.get_inputList()[j].From_Block.empty()) {
+						std::string InputBlockVarName = CurDFG.get_inputList()[j].InputBlockVarName;
+						DataFlowGraph* ToBlock = findvarfrom(InputBlockVarName);
+						// 整理DFGs中的数据流关系，并根据DFG中的opList生成DFG的outputList
+						ToBlock->get_outputList().push_back(OutputEdge(CurDFG.get_label(), InputBlockVarName));
+						CurDFG.get_inputList()[j].From_Block = ToBlock->get_label();
 					}
+					
 				}
 			}
 			// inputList生成完毕，至此，所有的依赖已经完成
+
 		}
 
 		// Operation
 		// 这个操作将返回NextNode在graph_node中的索引数组
 		std::vector<int> NextNode(std::string label) {
-			std::vector<int> nn;
 			int index = IndexofDFG[label];
-			for (int i = 0; i < DFGs[index].controlnodes.size(); i++) {
-				nn.push_back(IndexofDFG[DFGs[index].controlnodes[i].To_Block]);
-			}
-			return nn;
+			return NextNode(index);
 		}
 		std::vector<int> NextNode(int index) {
 			std::vector<int> nn;
@@ -93,15 +93,15 @@ class ControlFlowGraph {
 			}
 			return nn;
 		}
-		std::string findvarfrom(std::string VarName) {
+		DataFlowGraph* findvarfrom(std::string VarName) {
 			for (int i = 0; i < DFGs.size(); i++) {
 				// 如果在块的输出变量表中找到了变量，那么就将这个块的标签返回
 				std::unordered_map<std::string, int> &map = DFGs[i].DFG.myOutvartable();
 				if (map.find(VarName) != map.end() && map[VarName] != 0) {
-					return DFGs[i].DFG.get_label();
+					return &DFGs[i].DFG;
 				}
 			}
-			return "CANNOTFIND";
+			return NULL;
 		}
 
 };
