67978caf682c879c3163b4081a78498116756cc9
shenxh-410 <130371034+shenxh-410@users.noreply.github.com>
2023-05-06 12:34:19 +0800

    Update Hungarian_algorithm.h

--- Hungarian_algorithm.h
+++ Hungarian_algorithm.h
@@ -1,14 +1,13 @@
 #ifndef HUNGARIAN_H
 #define HUNGARIAN_H
-
 #include"leftAlgorithm.h"
-using namespace std;
+
 
 
 //寻找特定DFG下的变量对应的寄存器
 //REGi是对应特定DFG的寄存器与变量绑定结果
-int findregister(vector<pair<string,int>> REGi,string val) {
-	for (vector<pair<string, int>>::iterator it = REGi.begin(); it != REGi.end();it++ ) {
+int findregister(std::vector<std::pair<std::string, int>> REGi, std::string val) {
+	for (std::vector<std::pair<std::string, int>>::iterator it = REGi.begin(); it != REGi.end(); it++) {
 		if (it->first == val) {
 			return it->second;
 		}
@@ -16,23 +15,23 @@ int findregister(vector<pair<string,int>> REGi,string val) {
 	return -1;
 }
 //寻找特定DFG下寄存器对应的变量名
-string findval(vector<pair<string, int>>REGi, int reg) {
-	for (vector<pair<string, int>>::iterator it = REGi.begin(); it != REGi.end(); it++) {
-		if (it->second == reg) {
-			return it->first;
-		}
-	}
-	cout << "cannot find the val" << endl;
-	return NULL;
-}
+//string findval(vector<pair<string, int>>REGi, int reg) {
+//	for (vector<pair<string, int>>::iterator it = REGi.begin(); it != REGi.end(); it++) {
+//		if (it->second == reg) {
+//			return it->first;
+//		}
+//	}
+//	cout << "cannot find the val" << endl;
+//	return NULL;
+//}
 
 //匹配方法：通过计算资源的输出寄存器进行匹配
-vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, vector<pair<string, int>>REGi, vector<computeresource>& CORE) {
-	vector<pair<int, int>> ans;
+std::vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, std::vector<std::pair<std::string, int>>REGi, std::vector<computeresource>& CORE) {
+	std::vector<std::pair<int, int>> ans;
 	//标号
 	int num = -1;
 	//从头到尾遍历oplist，除去头部的空op和尾部的空op不用绑定计算资源
-	for (vector<node>::iterator it = DFG.get_opList().begin() + 1; it != DFG.get_opList().end() - 1; it++) {
+	for (std::vector<node>::iterator it = DFG.get_opList().begin() + 1; it != DFG.get_opList().end() - 1; it++) {
 		num++;
 		int optype = it->element.getOPtype();
 		int reg = findregister(REGi, it->element.getOutputvar());
@@ -75,7 +74,7 @@ vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, vector<pair<
 			if (CORE[i].outputregister == reg && resourcetype == CORE[i].flag) {//找到了输出寄存器匹配的对应计算资源
 				//进行时序判断
 				int flagtimejudgement = 0;
-				for (vector<pair<int, int>>::iterator it2 = ans.begin(); it2 != ans.end(); it2++) {
+				for (std::vector<std::pair<int, int>>::iterator it2 = ans.begin(); it2 != ans.end(); it2++) {
 					if (it2->second == i) {//说明该DFG之前有匹配过该计算资源
 						if (DFG.get_opList()[it2->first].getTstart() > it->getTend() || DFG.get_opList()[it2->first].getTend() < it->getTstart()) {//满足时序
 							continue;
@@ -87,7 +86,7 @@ vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, vector<pair<
 				}
 				if (flagtimejudgement == 0) {//匹配到之前已经实例化的计算资源
 					//更改输入寄存器（默认node的InputerVar只有一个或两个）
-					for (vector<string>::iterator it3 = it->InputVar.begin(); it3 != it->InputVar.end(); it3++) {
+					for (std::vector<std::string>::iterator it3 = it->InputVar.begin(); it3 != it->InputVar.end(); it3++) {
 						if (1 == it->InputVar.size()) {
 							int reg1 = findregister(REGi, it->InputVar[0]);
 							//cout << "reg1:" << reg1 << endl;
@@ -124,7 +123,7 @@ vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, vector<pair<
 
 						}
 					}
-					ans.push_back(make_pair(num, i));
+					ans.push_back(std::make_pair(num, i));
 					flag1 = 1;
 					//cout << "已存在计算资源，并完成分配计算资源" << endl;
 				}
@@ -145,7 +144,7 @@ vector<std::pair<int, int>> bindcomputeresource(DataFlowGraph& DFG, vector<pair<
 				com.setinputBregisters(findregister(REGi, it->InputVar[1]));
 				//cout << "reg2:" << findregister(REGi, it->InputVar[1]) << endl;
 			}
-			ans.push_back(make_pair(num, CORE.size()));
+			ans.push_back(std::make_pair(num, CORE.size()));
 			CORE.push_back(com);
 			//cout << "未匹配到旧有计算资源，新创建一个计算资源" << endl;
 		}
