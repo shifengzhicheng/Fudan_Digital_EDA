d576cec54eb5dd02c06ed5c2141996753b07c7f4
4xiang4 <130303173+4xiang4@users.noreply.github.com>
2023-05-13 10:18:33 +0800

    Update tb_gcd.v

--- testfile/tb_gcd.v
+++ testfile/tb_gcd.v
@@ -1,43 +1,37 @@
 `timescale 1ns / 1ps
 
 module tb_gcd;
+	reg [31:0] a;
+	reg [31:0] b;
+	wire [31:0] ap_return;
+	reg ap_clk = 0;
+	reg ap_rst_n;
+	reg ap_start;
+	wire ap_idle;
+	wire ap_done;
 
-  // Declare inputs and outputs
-  reg [31:0] a;
-  reg [31:0] b;
-  wire [31:0] ap_return;
-  reg ap_clk = 0;
-  reg ap_rst_n;
-  reg ap_start;
-  wire ap_idle;
-  wire ap_done;
+    gcd test_gcd(
+    .a(a),
+    .b(b),
+    .ap_return(ap_return),
+	.ap_clk(ap_clk),
+    .ap_rst_n(ap_rst_n),
+	.ap_start(ap_start),
+	.ap_idle(ap_idle),
+	.ap_done(ap_done)
+    );
 
-  // Instantiate the DUT
-  gcd gcd_test (
-      .a(a),
-      .b(b),
-      .ap_return(ap_return),
-      .ap_clk(ap_clk),
-      .ap_rst_n(ap_rst_n),
-      .ap_start(ap_start),
-      .ap_idle(ap_idle),
-      .ap_done(ap_done)
-  );
-
-  // Clock generation
-  always #5 ap_clk = ~ap_clk;
-
-  // Reset generation
-  initial begin
-    ap_rst_n = 0;
-    ap_start = 1;
-    a = 32'd24;
-    b = 32'd56;
-    #10;
-    ap_rst_n = 1;
-
-  end
-
-  // Test case
+    // Clock generation
+    always #5 ap_clk = ~ap_clk;
+    
+    // Reset generation
+    initial begin
+        ap_rst_n = 0;
+        ap_start = 1;
+        a = 32'd24;
+        b = 32'd56;
+        #10;
+        ap_rst_n = 1;
+    end
 
 endmodule
